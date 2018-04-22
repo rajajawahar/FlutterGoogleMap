9518d7f408a381f009c843468955bfbaba7c1f7b
Raja Jawahar <rajajawahar77@gmail.com>
2018-04-22 22:55:47 +0530

    Initial Commit

---
+++ android/app/src/main/java/com/example/googlemapflutter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.googlemapflutter;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
